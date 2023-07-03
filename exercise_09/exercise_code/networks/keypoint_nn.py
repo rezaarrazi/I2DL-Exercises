870aaa61a088acfd1b4f208636cac82309823e80
rezaarrazi <rezaarrazi@gmail.com>
2023-07-03 15:14:13 +0200

    ex9 init

--- exercise_09/exercise_code/networks/keypoint_nn.py
+++ exercise_09/exercise_code/networks/keypoint_nn.py
@@ -34,36 +34,7 @@ class KeypointModel(nn.Module):
         ########################################################################
         
 
-        # Define the layers
-        self.conv1 = nn.Conv2d(1, 16, 4)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.drop1 = nn.Dropout(hparams['dropout'])
-        self.act1 = nn.ReLU()
-        
-        self.conv2 = nn.Conv2d(16, 32, 3)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.drop2 = nn.Dropout(hparams['dropout'])
-        self.act2 = nn.ReLU()
-        
-        self.conv3 = nn.Conv2d(32, 64, 2)
-        self.pool3 = nn.MaxPool2d(2, 2)
-        self.drop3 = nn.Dropout(hparams['dropout'])
-        self.act3 = nn.ReLU()
-        
-        self.conv4 = nn.Conv2d(64, 128, 1)
-        self.pool4 = nn.MaxPool2d(2, 2)
-        self.drop4 = nn.Dropout(hparams['dropout'])
-        self.act4 = nn.ReLU()
-        
-        self.fc1 = nn.Linear(128*5*5, 500)
-        self.drop5 = nn.Dropout(hparams['dropout'])
-        self.act5 = nn.ReLU()
-        
-        self.fc2 = nn.Linear(500, 500)
-        self.drop6 = nn.Dropout(hparams['dropout'])
-        self.act6 = nn.ReLU()
-        
-        self.fc3 = nn.Linear(500, 30)
+        pass
 
         ########################################################################
         #                           END OF YOUR CODE                           #
@@ -82,16 +53,7 @@ class KeypointModel(nn.Module):
         ########################################################################
 
 
-        x = self.drop1(self.pool1(self.act1(self.conv1(x))))
-        x = self.drop2(self.pool2(self.act2(self.conv2(x))))
-        x = self.drop3(self.pool3(self.act3(self.conv3(x))))
-        x = self.drop4(self.pool4(self.act4(self.conv4(x))))
-        
-        x = x.view(x.size(0), -1)
-        
-        x = self.drop5(self.act5(self.fc1(x)))
-        x = self.drop6(self.act6(self.fc2(x)))
-        x = self.fc3(x)
+        pass
 
         ########################################################################
         #                           END OF YOUR CODE                           #
